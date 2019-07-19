a2faebff6ced349248b986230722e0e0b6e6f993
gaoyong <gaoyong@howzhi.com>
2019-07-19 10:55:57 +0800

    fix

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-module.exports.host = 'https://leixiaoguan.browqui.com' // dev
-// module.exports.host = 'https://yslxg.gj519.com' // prod
+// module.exports.host = 'https://leixiaoguan.browqui.com' // dev
+module.exports.host = 'https://yslxg.gj519.com' // prod
